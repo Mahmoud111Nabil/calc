cb9763bb947ab735feb10498e6d2b493b590fc2d
Mahmoud111Nabil <mahmoudnabil1289p@gmail.com>
2024-09-16 16:21:32 +0300

    dfwfed

---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CalculatorApp());
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String output = "0";
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "CLEAR") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "/" ||
+        buttonText == "x") {
+      num1 = double.parse(output);
+      operand = buttonText;
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already contains a decimal");
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "x") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ElevatedButton(
+          onPressed: () => buttonPressed(buttonText),
+          style: ElevatedButton.styleFrom(
+              minimumSize: Size(100, 100),
+              maximumSize: Size(400, 400),
+              backgroundColor: Colors.black, // Background color
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)) // Circular shape
+
+              ),
+          child: Text(
+            buttonText,
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 59, 62, 156), // Text color
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Calculator",
+          style: TextStyle(color: Colors.blueGrey),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              output,
+              style: TextStyle(fontSize: 49.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Column(children: [
+            Row(children: [
+              buildButton("7"),
+              buildButton("8"),
+              buildButton("9"),
+              buildButton("/")
+            ]),
+            Row(children: [
+              buildButton("4"),
+              buildButton("5"),
+              buildButton("6"),
+              buildButton("x")
+            ]),
+            Row(children: [
+              buildButton("1"),
+              buildButton("2"),
+              buildButton("3"),
+              buildButton("-")
+            ]),
+            Row(children: [
+              buildButton("."),
+              buildButton("0"),
+              buildButton("00"),
+              buildButton("+")
+            ]),
+            Row(children: [
+              buildButton("CLEAR"),
+              buildButton("="),
+            ]),
+          ])
+        ],
+      ),
+    );
+  }
+}
